2fb635a214f63437271f9c63b87faee698d98c19
vignesh57001 <vigneshmenon2303@gmail.com>
2023-03-07 22:37:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const menu = document.querySelector("nav .container ul");
+menuBtn.addEventListener("click", () => {
+  menu.style.display = "block";
+  menuBtn.style.display = "none";
+  closeBtn.style.display = "inline-block";
+});
+closeBtn.addEventListener("click", () => {
+  menu.style.display = "none";
+  menuBtn.style.display = "inline-block";
+  closeBtn.style.display = "none";
+});
+const navItems = menu.querySelectorAll("li");
+const changeActiveItem = () => {
+  navItems.forEach((item) => {
+    const link = item.querySelector("a");
+    link.classList.remove("active");
+  });
+};
+navItems.forEach((item) => {
+  const link = item.querySelector("a");
+  link.addEventListener("click", () => {
+    changeActiveItem();
+    link.classList.add("active");
+  });
+});
